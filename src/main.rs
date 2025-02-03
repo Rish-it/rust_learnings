0e224ca0dd4699448e3dcec193f17a339ff3e881
Rish-it <201b214@juetguna.in>
2025-02-03 23:58:39 +0530

    rust basics

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+// for intergers whether positive or negative we have i8, i16, i32, i64, i128, isize and for decimals/flaot defualt is f64.
+ let _x:i16 =10;
+ let _y:i32 =20;
+ let _z:i64 =30;
+
+ print!("x:{}", _z);
+
+}

7c8ca9686b3f0d953cc144bcbe95c6cf60d85d78
Rish-it <201b214@juetguna.in>
2025-02-04 00:12:22 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-fn main() {
+// // fn main() {
 
-// for intergers whether positive or negative we have i8, i16, i32, i64, i128, isize and for decimals/flaot defualt is f64.
- let _x:i16 =10;
- let _y:i32 =20;
- let _z:i64 =30;
+// // // for intergers whether positive or negative we have i8, i16, i32, i64, i128, isize and for decimals/flaot defualt is f64.
+// //  let _x:i16 =10;
+// //  let _y:i32 =20;
+// //  let _z:i64 =30;
 
- print!("x:{}", _z);
+// //  print!("x:{}", _z);
 
-}
+// // }
+
+
+// fn main(){
+//     let ax: &str = "Hello";
+
+//     //fixed type, //vectors // it can change the space at runtime ex- as you append the string. 
+// }
+
+
+
+
